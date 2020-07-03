4d94231024534cd9e6e26dd26d61863a2187e98d
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-07-03 12:42:40 +0200

    create the function to move the logs

--- app.js
+++ app.js
@@ -78,4 +78,64 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //move the logs
+    function autoMoveLogs() {
+        logsLeft.forEach(logsLeft => moveLogLeft(logsLeft))
+        logsRight.forEach(logsRight => moveLogRight(logsRight))
+    }
+
+    //move the log left on a time loop
+    function moveLogLeft() {
+        switch (true) {
+            case logLeft.classList.contains('l1'):
+                logLeft.classList.remove('l1')
+                logLeft.classList.add('l2')
+                break
+            case logLeft.classList.contains('l2'):
+                logLeft.classList.remove('l2')
+                logLeft.classList.add('l3')
+                break
+            case logLeft.classList.contains('l3'):
+                logLeft.classList.remove('l3')
+                logLeft.classList.add('l4')
+                break
+            case logLeft.classList.contains('l4'):
+                logLeft.classList.remove('l4')
+                logLeft.classList.add('l5')
+                break
+            case logLeft.classList.contains('l5'):
+                logLeft.classList.remove('l5')
+                logLeft.classList.add('l1')
+                break
+        }
+    }
+
+    //move the log right on a time loop
+    function moveLogRight() {
+        switch (true) {
+            case logRight.classList.contains('l1'):
+                logRight.classList.remove('l1')
+                logRight.classList.add('l5')
+                break
+            case logRight.classList.contains('l2'):
+                logRight.classList.remove('l2')
+                logRight.classList.add('l1')
+                break
+            case logRight.classList.contains('l3'):
+                logRight.classList.remove('l3')
+                logRight.classList.add('l2')
+                break
+            case logRight.classList.contains('l4'):
+                logRight.classList.remove('l4')
+                logRight.classList.add('l3')
+                break
+            case logRight.classList.contains('l5'):
+                logRight.classList.remove('l5')
+                logRight.classList.add('l4')
+                break
+        }
+    }
+
+    //rules to win frogger
+
 })
